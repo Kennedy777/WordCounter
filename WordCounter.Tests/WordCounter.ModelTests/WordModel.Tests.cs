a533c34aac9bfa0e1d9c143c8a6bbea81eaab634
Kennedy777 <reach.marguerite@gmail.com>
2019-05-03 16:46:45 -0700

    debug Controller files

--- WordCounter.Tests/WordCounter.ModelTests/WordModel.Tests.cs
+++ WordCounter.Tests/WordCounter.ModelTests/WordModel.Tests.cs
@@ -1,6 +1,6 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WordCounter.Solution;
+using System;
 using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WordCounter.Models;
 
 namespace WordCounter.Models.Tests

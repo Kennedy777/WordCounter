56190981c9d51a5c42a061bdac5e67dc0e18f788
Kennedy777 <reach.marguerite@gmail.com>
2019-05-05 18:05:42 -0700

    addbackground images and jumbotron

--- WordCounter.Tests/WordCounter.ModelTests/WordModel.Tests.cs
+++ WordCounter.Tests/WordCounter.ModelTests/WordModel.Tests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WordCounter.Models;
+// using System.Web.Mvc;
 
 namespace WordCounter.Models.Tests
 {
@@ -12,13 +13,13 @@ namespace WordCounter.Models.Tests
     [TestMethod]
     public void WordCount_FindWordsinString_5()
     {  
-       Assert.AreEqual(5, WordCounter.CountWords("buffalo", "Buffalo buffalo buffalo Buffalo buffalo."));
+       Assert.AreEqual(5, WordModel.CountWords("buffalo", "Buffalo buffalo buffalo Buffalo buffalo"));
     }
 
     [TestMethod]
     public void WordCount_FindWordsInStringWithCommas_2()
     {
-       Assert.AreEqual(2, WordCounter.CountWords("cats", "Some cats are good, others are not cats.");        )
+       Assert.AreEqual(2, WordModel.CountWords("cats", "Some cats are good, others are not cats"));        
     }
   }
 }
